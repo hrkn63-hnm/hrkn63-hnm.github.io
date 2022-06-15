ef3c255e6606e4a329ca9939cb0fb88b4de1f508
hrkn63-hnm <hrkn63-hnm@users.noreply.github.com>
2022-06-15 15:09:04 +0000

    deploy: faf563a276f8ab2c62c0b93afc1f1728106d9171

--- _next/static/DPEeKG1OJ-XDfIZOG0eyL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f5660ae40e608d92.js"],"/_error":["static/chunks/pages/_error-635a6533d6ac3ca1.js"],"/linkSample":["static/chunks/pages/linkSample-bb6b166f6b4c3b3c.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
