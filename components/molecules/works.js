f6aed4381a8256c9eb44ebf8ddb56f1af33d24cb
hnm876_md <demo43160@gmail.com>
2022-06-17 21:50:21 +0900

    Worksのコンポーネントをもう一つ呼び出した

--- components/molecules/works.js
+++ components/molecules/works.js
@@ -22,6 +22,12 @@ export default function Works(props) {
           text="テスト中ですので悪しからず"
           date="2022年6月16日"
         />
+        <Worksheet
+          img="https://cdn.discordapp.com/attachments/757627473427955867/986973635984109569/169.jpg"
+          title="テスト"
+          text="テスト中ですので悪しからず"
+          date="2022年6月16日"
+        />
       </div>
     </div>
   );
