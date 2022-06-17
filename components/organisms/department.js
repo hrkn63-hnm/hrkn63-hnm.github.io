59027ec692206fc7e87cb615499933def8189b17
hnm876_md <demo43160@gmail.com>
2022-06-18 00:58:06 +0900

    小見出しの大きさを変えてdisplay-5にした

--- components/organisms/department.js
+++ components/organisms/department.js
@@ -1,7 +1,7 @@
 export default function Department(props) {
   return (
     <div>
-      <h4 className="mb-3 mt-3 pb-3 pt-3">{props.title}</h4>
+      <h4 className="mb-3 mt-3 pb-3 pt-3 display-5">{props.title}</h4>
       <p>{props.paragraph}</p>
       <p>{props.paragraph2}</p>
       <p>{props.paragraph3}</p>
