c84ede80a4ee256c4b78c7c537f45f69ef2dc64e
hrkn63-hnm <demo43160@gmail.com>
2022-07-09 14:09:04 +0900

    blog1を書いた/yarnのinstall

--- components/molecules/blog.js
+++ components/molecules/blog.js
@@ -1,20 +1,25 @@
 import { Card } from "react-bootstrap";
 import { ListGroup } from "react-bootstrap";
+import Link from "next/link";
 
-export default function Blog(props) {
+
+export default function Blog() {
   return (
     <div>
       <Card style={{ width: "rem" }}>
         <Card.Header>一覧</Card.Header>
         <ListGroup variant="flush">
           <ListGroup.Item>
-            <a href="">今まで生きてきておそらく一番嬉しかったことが文化祭で起きた話</a>
+            <Link href="/blog/1">
+              <a>[1]今まで生きてきておそらく一番嬉しかったことが文化祭で起きた話</a>
+            </Link>
           </ListGroup.Item>
           <ListGroup.Item>
-            <a href="">ふざけて書いた童話が全国最優秀賞になってしまった話</a>
+            <Link href="/">
+              <a>[2]ふざけて書いた童話が全国最優秀賞になってしまった話</a>
+            </Link>
           </ListGroup.Item>
           <ListGroup.Item>
-            <a href=""></a>
           </ListGroup.Item>
         </ListGroup>
       </Card>
